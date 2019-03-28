2fbeb4e17d0de6b08f56944a2e51d82c541a3d53
Jeremy Green <jmgamer12@users.noreply.github.com>
2019-03-27 23:31:17 -0500

    Created models. Turns out you can create tables in  Django

--- CarFinder/settings.py
+++ CarFinder/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'CarFinder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        #'NAME': os.path.join(BASE_DIR, 'db.mysql'),
+        'NAME': 'carfinder',
+        'USER': 'root',
+        'PASSWORD': 'carfinder',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
--- FindCar/models.py
+++ FindCar/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 # Create your models here.
+
+
+class Person(models.Model):
+    PID = models.ForeignKey('Driver', verbose_name="PersonID", primary_key=True, on_delete=models.CASCADE, unique=True)
+    p_name = models.CharField("Person's Name", max_length=45)
+    phone = models.PositiveIntegerField("Phone Number", max_length=9)
+    team = models.CharField("SubTeam", max_length=45)
+    departTime = models.CharField(verbose_name="DepartTime", max_length=9)
+
+
+class Event(models.Model):
+    EID = models.ForeignKey('Person', verbose_name="EventID", primary_key=True, unique=True, on_delete=models.CASCADE)
+    time = models.DateTimeField()
+    location = models.CharField("Location", max_length=45)
+
+
+class Car(models.Model):
+    CID = models.ForeignKey('Driver', verbose_name="CarID", primary_key=True, on_delete=models.CASCADE, unique=True)
+    numSeats = models.PositiveIntegerField("#Seats")
+    timeDepart = models.DateTimeField()
+
+class Driver(models.Model):
+    pass
+
+class Organization(models.Model):
+    org_name = models.ForeignKey(Person, verbose_name="Name", on_delete=models.CASCADE)
